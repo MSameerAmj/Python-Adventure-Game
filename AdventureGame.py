1df30dd8438c13d727a40dcca9d1cde29d0b4f69
Ashir64 <ashirkhizerkhan@gmail.com>
2024-10-01 19:26:49 +0500

    uptaded look and take functions

--- AdventureGame.py
+++ AdventureGame.py
@@ -1,13 +1,32 @@
-def look ():
+def look():
     global curr_loc
+    print('==> ', end='')
     print(map[curr_loc]['description'])
-    for item in map[curr_loc]['items']:
-        print(Items_in_game[item]['description'])
+    print()
+    if map[curr_loc]['items']:
+        for item in map[curr_loc]['items']:
+            print('==> ', end='')
+            print(Items_in_game[item]['description'])
+    else:
+        print('==> ', end='')
+        print(f'No item to explore in {curr_loc}')
+        
 
+        
 def take(item):
     global curr_loc
-    if item in map[curr_loc]['items'] :
-        inventory.append(item)
-        print(f'{item} added to inventory')
+    if item in Items_in_game:
+        if Items_in_game[item]['status'] is not ItemState.CANNOTUSE:
+            if item not in map[curr_loc]['items'] :
+                print('==> ', end='')
+                print("Item not in the room! ")
+            else:
+                inventory.append(item)
+                print('==> ', end='')
+                print(f'{item} added to inventory')
+        else:
+            print('==> ', end='')
+            print(f'Cannot add {item} in inventory')
     else:
-        print("Item not in the room! ")    
+        print('==> ', end='')
+        print("Item not in the game!")
